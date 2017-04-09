71fd57a1a39ed70c7ba1c448ea0a9877416b7c8f
Va-leri <v.budilova@mail.ru>
2017-04-09 11:15:27 +0300

    Исправила по замечаниям

--- js/stat.js
+++ js/stat.js
@@ -8,42 +8,55 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
   //надпись
-  ctx.fillStyle = '#000';
-  ctx.font = '16px, \"PT Mono\"';
-  ctx.fillText('Ура, вы победили!', 110, 45);
-  ctx.fillText('Список результатов:', 110, 65);
+  var writeText = function(text, x, y) {
+    ctx.fillStyle = '#000';
+    ctx.font = '16px, \"PT Mono\"';
+    ctx.fillText(text, x, y); 
+  };
+  
+  writeText('Ура, вы победили!', 110, 45);
+  writeText('Список результатов:', 110, 65);
+  
    
   //определяем максимальное время
   var maxTime = times[0];
-  for (var i=0; i < times.length; i++){
-    if (maxTime < times[i]) {
-      maxTime = times[i];
-    } 
-  }
+  (function() {
+    for (var i=0; i < times.length; i++){
+      if (maxTime < times[i]) {
+        maxTime = times[i];
+      } 
+    }
+  } ());  
   
-  //гистограмма
-  var histogramHeight = 150;
-  var histogramYMin = 95;
-  var columnWidth = 40;
-  var histogramXMin = 130;
-  var intercolumnWidth = 50;
-  var histogramYMax = histogramYMin + histogramHeight;
-  
-  //alert(times);
-  //ctx.fillText('max time=' + maxTime, 110, 85);
-  
-  for(var i = 0; i < times.length; i++){
-    var columnHeight = histogramHeight/maxTime*times[i];
-    var getColumnColour = function() {
-      if (names[i] === 'Вы') {
+  var i;
+  var getColumnColour = function(name){
+      if (name === 'Вы') {
       return 'rgba(255, 0, 0, 1)';
     } else {
       return 'rgba(0,0,255,' + Math.random() + ')';
-    }}
-    ctx.fillStyle = getColumnColour();
-    ctx.fillRect(histogramXMin + i * (columnWidth + intercolumnWidth) , histogramYMin + histogramHeight - columnHeight, 40, columnHeight);
-    ctx.fillStyle = '#000';
-    ctx.fillText(times[i].toFixed(0), histogramXMin + i * (columnWidth + intercolumnWidth), histogramYMin + histogramHeight - columnHeight - 10);
-    ctx.fillText(names[i], histogramXMin + i * (columnWidth + intercolumnWidth), histogramYMax + 20)
+    }};
+  //гистограмма
+  var histogram = {
+    height : 150,
+    columnWidth : 40,
+    yMin : 95,
+    xMin : 130,
+    interColumnWidth : 50
+  };
+  
+  histogram.yMax =  histogram.yMin + histogram.height;
+  
+  for(var i = 0; i < times.length; i++){
+    histogram.columnHeight = histogram.height / maxTime * times[i];
+    
+    histogram.draw = function () {
+      ctx.fillStyle = getColumnColour(names[i]);
+      ctx.fillRect(histogram.xMin + i * (histogram.columnWidth + histogram.interColumnWidth) , histogram.yMin + histogram.height - histogram.columnHeight, 40, histogram.columnHeight);
+    };
+    histogram.writeTime = writeText(times[i].toFixed(0), histogram.xMin + i * (histogram.columnWidth + histogram.interColumnWidth), histogram.yMin + histogram.height - histogram.columnHeight - 10);
+    histogram.writeName = writeText(names[i], histogram.xMin + i * (histogram.columnWidth + histogram.interColumnWidth), histogram.yMax + 20);
+    
+    histogram.draw();
+  
   };
 };
